38c3f63c6f9aaee7f6ac0bbde30ec3a592556693
oliandrik <os.shcherbakovaa@gmail.com>
2023-01-05 11:43:20 +0200

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import * as dotenv from 'dotenv';
+import { resolve } from 'path';
 import 'reflect-metadata';
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-dotenv.config({ path: '../.env' });
+dotenv.config({ path: resolve('../.env') });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
